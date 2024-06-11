2f0dd70a28c64075d3bad1d058490ccbc423075b
CH-Vitalij <v.charkin2017@gmail.com>
2024-06-11 17:44:38 +0500

    релизовано выпадающее меню, + добавлено событие click на репозитории

--- index.js
+++ index.js
@@ -1,22 +1,67 @@
+const form = document.getElementById("formElem");
+const input = document.getElementById("autocomplete");
+const menuRepositories = document.querySelector(".menu-repositories");
+const menuRepositoriesItem = document.querySelectorAll(
+  ".menu-repositories__item"
+);
+
+let json;
+
+input.addEventListener("input", debounce(handleRequest, 500));
+menuRepositories.addEventListener("click", addRepositories);
+
 async function handleRequest(evt) {
   evt.preventDefault();
 
   let value = input.value;
 
-  let url = `https://api.github.com/search/repositories?q=${value}+in:name&sort=best-match&order=desc`;
+  if (value.charCodeAt() !== 32 && value !== "") {
+    let url = `https://api.github.com/search/repositories?q=${value}+in:name&sort=best-match&order=desc`;
+    let response = await fetch(url);
 
-  let response = await fetch(url);
-  if (response.ok) {
-    let json = await response.json();
-    console.log(json);
-    // console.log(json.items.map((el) => el.name.sort()));
+    if (response.ok) {
+      json = await response.json();
+      // console.log(json);
 
-    // console.log(json.items[0].name);
-    // console.log(json.items[0].owner.login);
-    // console.log(json.items[0].stargazers_count);
-  } else {
-    alert("Ошибка HTTP: " + response.status);
+      setListContent(json);
+
+      menuRepositories.style.opacity = 1;
+      menuRepositories.style.visibility = "visible";
+    } else {
+      alert("Ошибка HTTP: " + response.status);
+    }
+  } else if (value === "") {
+    menuRepositoriesItem.forEach((el) => (el.textContent = ""));
+    menuRepositories.style.opacity = 0;
+    menuRepositories.style.visibility = "hidden";
   }
+}
+
+function setListContent(json) {
+  const len = json.items.length;
+  menuRepositoriesItem.forEach((el, i) => {
+    if (len >= 5) {
+      el.textContent =
+        json.items[i].name[0].toUpperCase() + json.items[i].name.slice(1);
+      el.dataset.show = "visible";
+    } else if (len < 5 && len !== 0) {
+      if (i >= len) {
+        el.textContent = "";
+        el.dataset.show = "hidden";
+      } else {
+        el.textContent =
+          json.items[i].name[0].toUpperCase() + json.items[i].name.slice(1);
+      }
+    } else if (len === 0) {
+      if (i === 0) {
+        el.textContent = "The repository with this name does not exist";
+        el.dataset.show = "visible";
+      } else {
+        el.textContent = "";
+        el.dataset.show = "hidden";
+      }
+    }
+  });
 }
 
 function debounce(fn, debounceTime) {
@@ -30,9 +75,12 @@ function debounce(fn, debounceTime) {
   };
 }
 
-const form = document.getElementById("formElem");
-const input = document.getElementById("autocomplete");
-
-console.log(input);
-
-input.addEventListener("input", debounce(handleRequest, 500));
+function addRepositories(evt) {
+  if (evt.target.tagName === "LI") {
+    let index = [...menuRepositories.children].indexOf(evt.target);
+    console.log(index);
+    console.log(json.items[index].name);
+    console.log(json.items[index].owner.login);
+    console.log(json.items[index].stargazers_count);
+  }
+}
